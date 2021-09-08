d4c6978788714d03c2729198aae740cdccb3d894
ClawmanCat <clawmancat@gmail.com>
2021-09-08 15:18:35 +0200

    Basic WPF Project for DartTracker and Unit Test Project

---
+++ Unit Tests/ExampleTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests {
+    // TODO: Remove this once there are some actual tests here that can be used as examples.
+    [TestClass]
+    public class ExampleTest {
+        [TestMethod]
+        public void ExampleTestMethod() {
+            string melon_1 = "Watermelon";
+            string melon_2 = "Watermelon";
+
+            Assert.AreEqual(melon_1, melon_2);
+        }
+    }
+}
